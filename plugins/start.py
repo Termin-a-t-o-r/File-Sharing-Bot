be64dc2bd6c51df40fc6b53610b818b5f80cd437
Aaron Shaji <90400808+Termin-a-t-o-r@users.noreply.github.com>
2021-11-16 13:33:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -90,8 +90,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton('🎬 Channel', url='https://t.me/Film_Arcade_Official'),
+                    InlineKeyboardButton('♥️ Group', url= 'https://t.me/Film_Arcade_Chat')
                 ]
             ]
         )
